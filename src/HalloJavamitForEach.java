00fed9de84a0c5b8eea4388b727b56cbf3b8a9db
koglerm190074 <koglerm190074@sus.htl-grieskirchen.at>
2021-09-27 11:06:42 +0200

    zweite Aufgabe finish

---
+++ src/HalloJavamitForEach.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class HalloJavamitForEach {
+    private List<String> list;
+
+    public HalloJavamitForEach(List<String> list) {
+        this.list = list;
+    }
+
+    public void print(){
+        for (String s:list) {
+            System.out.println(s);
+        }
+        System.out.println("\n");
+
+        list.forEach((String s) -> System.out.println(s));
+        System.out.println("\n");
+
+        Consumer<String> consumer = (String s) -> System.out.println(s);
+        System.out.println("\n");
+
+        list.forEach(System.out::println);
+        System.out.println("\n");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+        List<String> list = new ArrayList<>();
+
+        list.add("A1");
+        list.add("B1");
+        list.add("C1");
+        list.add("D1");
+        list.add("E1");
+
+        HalloJavamitForEach hjmfe = new HalloJavamitForEach(list);
+
+        hjmfe.print();
+
+
     }
 }
